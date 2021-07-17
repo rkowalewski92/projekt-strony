2ff4ebf197166cc0f35ee662b0ddb2d111236eaf
rkowalewski92 <48176089+rkowalewski92@users.noreply.github.com>
2021-07-17 18:06:33 +0200

    17.07
    17.07

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$(function() {
+    var $inputs = $('form input[required], form textarea[required], select[required]');
+
+    var displayFieldError = function($elem) {
+        var $fieldRow = $elem.closest('.form-row');
+        var $fieldError = $fieldRow.find('.field-error');
+        if (!$fieldError.length) {
+            var errorText = $elem.attr('data-error');
+            var $divError = $('<div class="field-error">' + errorText + '</div>');
+            $fieldRow.append($divError);
+        }
+    };
+
+    var hideFieldError = function($elem) {
+        var $fieldRow = $elem.closest('.form-row');
+        var $fieldError = $fieldRow.find('.field-error');
+        if ($fieldError.length) {
+            $fieldError.remove();
+        }
+    };
+
+    $inputs.on('input keyup', function() {
+        var $elem = $(this);
+        if (!$elem.get(0).checkValidity()) {
+            $elem.addClass('error');
+        } else {
+            $elem.removeClass('error');
+            hideFieldError($elem);
+        }
+    });
+
+    $inputs.filter(':checkbox').on('click', function() {
+        var $elem = $(this);
+        var $row = $(this).closest('.form-row');
+        if ($elem.is(':checked')) {
+            $elem.removeClass('error');
+            hideFieldError($elem);
+        } else {
+            $elem.addClass('error');
+        }
+    });
+})
